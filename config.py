ff027c05005804f8c63bddaeef2c28cc28a6d66d
Radhebhaixyz <Sudama703749@gmail.com>
2025-04-25 00:40:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,12 +5,12 @@ from os import getenv
 API_ID = int(getenv("API_ID", "20346550"))
 API_HASH = getenv("API_HASH", "bc79c3bea7a626887bdc0871eecf0327")
 BOT_TOKEN = getenv("BOT_TOKEN", "7358697038:AAGJBd7zlkB66QnP_4XHD3Ww17ymxIUonq8")
-OWNER_ID = int(getenv("OWNER_ID", "7081036509"))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7081036509 7491167754").split()))
+OWNER_ID = int(getenv("OWNER_ID", "6999625105"))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6999625105 7731407856").split()))
 MONGO_URL = getenv("MONGO_DB", "mongodb+srv://daxxop:daxxop@daxxop.dg3umlc.mongodb.net/?retryWrites=true&w=majority")
 
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002278731669"))
-PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", "-1002278731669"))
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002638405121"))
+PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", "-1002501818785"))
 
 """
 #
